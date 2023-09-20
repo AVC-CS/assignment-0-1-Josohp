72cfec2ca029e2e7b7e3e29173702fe05cb2dbe3
Josohp <jadistan4@gmail.com>
2023-09-20 09:24:56 -0700

    Done

--- main.py
+++ main.py
@@ -6,6 +6,24 @@ def main():
     ##############################
     print ('Hello World')
 
+    # num1 = int(input('Enter the number 1 :'))
+    # num2 = int(input('Enter the number 2: '))
+    # num2 = int(input('Enter the number 3: '))
+    num1 = 5
+    num2 = 9
+    num3 = 1
+
+    if num1 < num2:
+        if num1 < num3:
+            minval = num1
+        else:
+            minval = num3
+    else:
+        if num2 < num3:
+            minval = num2
+        else:
+            minval = num3
+    print (minval)                        
 
 if __name__ == '__main__':
     main()
